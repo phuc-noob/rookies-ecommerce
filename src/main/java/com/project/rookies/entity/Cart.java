f20aeaa6880c7cee8efc64741a8ccb548c0163e9
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-12 12:18:13 +0700

    create: admin model

---
+++ src/main/java/com/project/rookies/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.project.rookies.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.engine.internal.Cascade;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_id", nullable = false)
+    private Long Id;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @OneToOne(cascade =CascadeType.ALL)
+    @JoinColumn(name = "customer_id",referencedColumnName = "id")
+    private Customer customer;
+    private boolean status;
+    @OneToMany(mappedBy = "cart")
+    private List<CartProduct> productList;
+}
