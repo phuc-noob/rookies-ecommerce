fe128605238d88395176bb8e5ef5e70b5ad0e5c1
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-12 12:09:38 +0700

    initialize project

---
+++ src/test/java/com/project/rookies/RookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
