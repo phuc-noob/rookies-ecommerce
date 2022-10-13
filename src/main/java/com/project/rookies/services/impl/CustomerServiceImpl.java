aea45ca505f28cba5839ce3df0ba259ddddf215c
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-13 21:27:59 +0700

    create impl service

--- src/main/java/com/project/rookies/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/project/rookies/services/impl/CustomerServiceImpl.java
@@ -1,2 +1,17 @@
-package com.project.rookies.services.impl;public class CustomerServiceImpl {
+package com.project.rookies.services.impl;
+
+import com.project.rookies.entities.Customer;
+import com.project.rookies.repositories.CustomerRepo;
+import com.project.rookies.services.inf.ICustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+    private final CustomerRepo customerRepo;
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
 }
--- src/main/java/com/project/rookies/services/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/project/rookies/services/impl/OrderDetailServiceImpl.java
@@ -1,2 +1,17 @@
-package com.project.rookies.services.impl;public class BillDetailService {
+package com.project.rookies.services.impl;
+
+import com.project.rookies.entities.OrderDetail;
+import com.project.rookies.repositories.OrderDetailRepo;
+import com.project.rookies.services.inf.IOrderDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderDetailServiceImpl implements IOrderDetailService {
+    private final OrderDetailRepo orderDetailRepo;
+    @Override
+    public OrderDetail saveOrderDetail(OrderDetail orderDetail) {
+        return orderDetailRepo.save(orderDetail);
+    }
 }
