7a901eb06d01aec7d2687cfaa8c13d9428d8075a
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-14 22:24:49 +0700

    update: customer service

--- src/main/java/com/project/rookies/services/inf/ICustomerService.java
+++ src/main/java/com/project/rookies/services/inf/ICustomerService.java
@@ -1,7 +1,14 @@
 package com.project.rookies.services.inf;
 
+import com.project.rookies.dto.request.CustomerDto;
+import com.project.rookies.dto.response.CustomerResponseDto;
 import com.project.rookies.entities.Customer;
 
+import java.util.List;
+
 public interface ICustomerService {
-    Customer saveCustomer(Customer customer);
+    CustomerResponseDto saveCustomer(CustomerDto customer);
+    boolean isExistCustomer(CustomerDto customerDto);
+    CustomerResponseDto updateCustomerById(CustomerDto customerDto,Long id);
+    List<CustomerResponseDto> findCustomerById(Long id);
 }
