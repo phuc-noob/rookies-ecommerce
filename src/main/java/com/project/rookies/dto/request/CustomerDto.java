0bb82e0470abfc5cc70f87ea645d2cf7f7a25dbc
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-14 22:18:43 +0700

    create dto layer

--- src/main/java/com/project/rookies/dto/request/CustomerDto.java
+++ src/main/java/com/project/rookies/dto/request/CustomerDto.java
@@ -1,2 +1,16 @@
-package com.project.rookies.dto.request;public class CustomerDto {
+package com.project.rookies.dto.request;
+
+import lombok.*;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class CustomerDto {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+    private String address;
+    private String gender;
+    private LocalDateTime dayOfBirth;
 }
