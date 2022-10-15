7922e3a1dbda5cbdfc4605ff623f165eacddd973
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-16 00:00:32 +0700

    update customer and billoder service

--- src/main/java/com/project/rookies/services/inf/IBillOrderService.java
+++ src/main/java/com/project/rookies/services/inf/IBillOrderService.java
@@ -1,7 +1,10 @@
 package com.project.rookies.services.inf;
 
+import com.project.rookies.dto.request.BillOrderDto;
+import com.project.rookies.dto.response.BillOrderResponseDto;
 import com.project.rookies.entities.BillOrder;
 
 public interface IBillOrderService {
-    BillOrder saveBillOrder(BillOrder billOrder);
+    BillOrderResponseDto saveBillOrder(BillOrderDto billOrder, Long customerId);
+    Boolean isExistBillOrder(Long customerId);
 }
--- src/main/java/com/project/rookies/services/inf/ICustomerService.java
+++ src/main/java/com/project/rookies/services/inf/ICustomerService.java
@@ -4,11 +4,14 @@ import com.project.rookies.dto.request.CustomerDto;
 import com.project.rookies.dto.response.CustomerResponseDto;
 import com.project.rookies.entities.Customer;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface ICustomerService {
     CustomerResponseDto saveCustomer(CustomerDto customer);
-    boolean isExistCustomer(CustomerDto customerDto);
     CustomerResponseDto updateCustomerById(CustomerDto customerDto,Long id);
     List<CustomerResponseDto> findCustomerById(Long id);
+    boolean isExistCustomer(CustomerDto customerDto);
+    void deleteCustomer(Long id, HttpServletResponse response);
+
 }
