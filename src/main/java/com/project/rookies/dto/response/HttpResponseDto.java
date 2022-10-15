3623dae2c69fe3401edb8be5e2171332703f189f
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-10-16 00:01:32 +0700

    create response and billorder dto

--- src/main/java/com/project/rookies/dto/response/HttpResponseDto.java
+++ src/main/java/com/project/rookies/dto/response/HttpResponseDto.java
@@ -1,13 +1,28 @@
 package com.project.rookies.dto.response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
-public class ApiResponse {
-    public static ResponseEntity<Object> responseMessage(String message, HttpStatus httpStatus)
-    {
-        ResponseDto responseDto = new ResponseDto(message,httpStatus.value(),httpStatus);
-        return new ResponseEntity<>(responseDto,HttpStatus.BAD_REQUEST);
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class HttpResponseDto {
+    public static void responseMessage(HttpServletResponse response, String message, HttpStatus httpStatus) {
+        try{
+            Map<String,Object> apiResponse = new HashMap<>();
+            apiResponse.put("statusCode",httpStatus.value());
+            apiResponse.put("message",message);
+            apiResponse.put("httpStatus",httpStatus);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(),apiResponse);
+        }catch (Exception ex)
+        {
+            throw new RuntimeException();
+        }
     }
 }
--- src/main/java/com/project/rookies/dto/response/ResponseDto.java
+++ src/main/java/com/project/rookies/dto/response/ResponseDto.java
@@ -1,2 +1,12 @@
-package com.project.rookies.dto.response;public class ResponseDto {
+package com.project.rookies.dto.response;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import java.time.ZonedDateTime;
+
+@RequiredArgsConstructor
+public class ResponseDto {
+    private final String message;
+    private final int statusCode;
+    private final HttpStatus httpStatus;
 }
